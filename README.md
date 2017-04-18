26a9c126802706cb629c10f186c2a2571ad57b13
David Djian <ddtraceweb@MacBook-Pro-de-David-2.local>
2017-04-18 17:27:19 +0200

    version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# sdk-api-php
+#Graphcomment SDK API PHP
+version 1.0
+
+
+- To use the Wrapper PHP you must have an account on Graphcomment.com and choose the business enterprise package.
+
+With this wrapper you can :
+ 
+ - Register a new User on Graphcomment when he subscribe on your website.
+ - Login a user on Graphcomment when he login on your website.
+ - Get the list of your users subscribe on Graphcomment.
+ - Get the thread by the Url of your subject with all comments to use for the SEO.
+ 
+ 
+ Example of implementation :
+ 
+ 
+ `$client = new Sdk(username|email, account_password, Graphcomment_website_id);`
+ 
+ `$client->authentification();`
+ 
+ after theses lines, you call the functions.
+ 
+ - register a User 
+ 
+ `$client->registerUser('username', 'email', 'fr');`
+ 
+ This function return a json contain his "gc_id" that must be save in your database to authenticate a user when log him.
+ 
+ - login a User
+ 
+`$client->loginUser('email', 'gc_id');`
+
+- Get the list of your users
+
+`$client->getUsersQuery("10", "1", "kevin")`
+
+get the list of users with name "kevin" subscribe to GC.
+
+- get the thread by url of your page without http or https
+
+`$client->getThread("//dev.graphcomment.com/demo");`
+
+return the json of the thread with all properties and comments.
---
+++ src/Graphcomment.php
@@ -0,0 +1,276 @@
+<?php
+/**
+ * Wrapper API Graphcomment v1.0.
+ * @author ddtraceweb <david@semiologic.fr>
+ * @copyright 2017 Graphcomment
+ * Date: 12/04/2017
+ * Time: 15:29
+ */
+
+namespace Graphcomment;
+
+use GuzzleHttp\Client;
+
+/**
+ * Class Sdk
+ * @package Graphcomment
+ */
+class Sdk
+{
+
+    /**
+     * @var
+     */
+    protected $username;
+    protected $password;
+    protected $token;
+    protected $websiteId;
+    protected $dir = 'https://graphcomment.com/api';
+
+    /**
+     * Sdk constructor.
+     * @param $username
+     * @param $password
+     * @param string $websiteId
+     */
+    public function __construct($username, $password, $websiteId = "")
+    {
+        $this->setUsername($username);
+        $this->setPassword($password);
+        $this->setWebsiteId($websiteId);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param mixed $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDir()
+    {
+        return $this->dir;
+    }
+
+    /**
+     * @param mixed $dir
+     */
+    public function setDir($dir)
+    {
+        $this->dir = $dir;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWebsiteId()
+    {
+        return $this->websiteId;
+    }
+
+    /**
+     * @param mixed $websiteId
+     */
+    public function setWebsiteId($websiteId)
+    {
+        $this->websiteId = $websiteId;
+    }
+
+
+    /**
+     * Authentification() this method use to authenticate your account to allow the SDK.
+     * You must call after the constructor settings and before all methods.
+     *
+     * @return string  json response JWT
+     */
+    public function authentification()
+    {
+        $client = new Client();
+        $res = $client->request('POST', $this->getDir() . '/users/login',
+            [
+                'form_params' => [
+                    'username' => $this->getUsername(),
+                    'password' => $this->getPassword()
+                ],
+                'http_errors' => false
+            ]);
+
+        if ($res->getStatusCode() == "200") {
+            $result = json_decode($res->getBody());
+
+            $this->setToken($result->token);
+        } else {
+            echo $res->getBody();
+        }
+    }
+
+    /**
+     * GetUsersQuery() this method call your members list and you can search by query and paginate the result.
+     *
+     * @param string $page_size
+     * @param string $page_nbr
+     * @param string $query
+     *
+     * @return string  json response user list
+     */
+    public function getUsersQuery($page_size = "10", $page_nbr = "1", $query = "user")
+    {
+        $client = new Client();
+        $res = $client->request('GET', $this->getDir() . '/users',
+            [
+                'headers' => [
+                    'Authorization' => 'JWT ' . $this->getToken()
+                ],
+                'query' => [
+                    'website_id' => $this->getWebsiteId(),
+                    'page_size' => $page_size,
+                    'page_nbr' => $page_nbr,
+                    'query' => $query
+                ],
+                'http_errors' => false
+            ]);
+
+        echo $res->getBody();
+    }
+
+    /**
+     * registerUser() Register a user to Graphcomment.
+     *
+     * @param string $username
+     * @param string $email
+     * @param string $language
+     *
+     * @return string  json response gc_id to store in your database
+     */
+    public function registerUser($username, $email, $language = "en")
+    {
+        $client = new Client();
+        $res = $client->request('POST', $this->getDir() . '/users/registerUserSdk',
+            [
+                'headers' => [
+                    'Authorization' => 'JWT ' . $this->getToken()
+                ],
+                'multipart' => [
+                    [
+                        'name' => 'username',
+                        'contents' => $username
+                    ],
+                    [
+                        'name' => 'email',
+                        'contents' => $email
+                    ],
+                    [
+                        'name' => 'language',
+                        'contents' => $language
+                    ],
+                    [
+                        'name' => 'from_website',
+                        'contents' => $this->getWebsiteId()
+                    ]
+                ],
+                'http_errors' => false
+            ]);
+
+        if ($res->getStatusCode() == "200") {
+            echo $res->getBody();
+        }
+        else {
+            echo $res->getBody();
+        }
+    }
+
+    /**
+     * loginUser() authenticate a user and return a token to login in graphcomment.
+     *
+     * @param string $email
+     * @param string $gc_id
+     *
+     * @return string  json JWT response
+     */
+    public function loginUser($email, $gc_id)
+    {
+        $client = new Client();
+        $res = $client->request('POST', $this->getDir() . '/users/loginSdk',
+            [
+                'headers' => [
+                    'Authorization' => 'JWT ' . $this->getToken()
+                ],
+                'form_params' => [
+                    'email' => $email,
+                    'gc_id' => $gc_id
+                ],
+                'http_errors' => false
+            ]);
+
+        echo $res->getBody();
+    }
+
+    /**
+     * getThread() Load the Thread of the url of the page with all comments of the thread for the SEO.
+     *
+     * @param string $url be "//domain/path" example //graphcomment.com/accueil.html
+     *
+     * @return string  json thread details
+     */
+    public function getThread($url)
+    {
+        $client = new Client();
+        $res = $client->request('GET', $this->getDir() . '/website/'.$this->getWebsiteId().'/Threadload',
+            [
+                'headers' => [
+                    'Authorization' => 'JWT ' . $this->getToken()
+                ],
+                'query' => [
+                    'website_id' => $this->getWebsiteId(),
+                    'url' => $url,
+                ],
+                'http_errors' => false
+            ]);
+
+        echo $res->getBody();
+    }
+}
